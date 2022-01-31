ad463ddbff9e8bdf9aeaf24776b5f0e5719cc4f9
Sayad Pervez <pervez2504@gmail.com>
2022-01-31 14:42:01 +0530

    butterworth completed

---
+++ Unit_3/Butterworth2/brain.js
@@ -0,0 +1,49 @@
+function getElement(id)
+{
+    return(document.getElementById(id));
+}
+
+function processData()
+{
+    getElement("output_container").scrollIntoView();
+    Ap = Number(getElement("input0").value);
+    As = Number(getElement("input1").value);
+    omegaP = Number(getElement("input2").value);
+    omegaS = Number(getElement("input3").value);
+    var T = Number(getElement("input4").value);
+    if(getElement("bilinear").checked)
+        transform = "Bilinear";
+    else if(getElement("impulseInvariant").checked)
+        transform = "Impulse Invariant";
+    else
+        getElement("output_container").innerHTML=`<h5 class="red-text bold">ERROR</h5>`
+    getElement("out_g").innerHTML=`<h6 class="indigo-text text-darken-4 bold">${transform} Transform</h6>Ap = ${Ap}<br><br>
+    As = ${As}<br><br>Ωp = ${omegaP}<br><br>Ωs = ${omegaS}<br><br>
+    `;
+    if(transform == "Bilinear")
+    {   //       Order of the filter
+        var N = ((1/2)* Math.log( ( (1 / Math.pow(As,2)) - 1 ) / ( (1 / Math.pow(Ap,2)) - 1 ) ) / Math.log(omegaS/omegaP)).toPrecision(5);
+        getElement("out_o").innerHTML = `<img class="materialboxed" src="./bilinear_images/n.jpeg" /><br><br>N = ${N} ~= ${Math.ceil(N)}<br><br>`;
+        N = Math.ceil(N);
+
+        //       Cut off Frequency
+        var Fc = (omegaP / Math.pow( ( (1 / Math.pow(Ap,2)) - 1 ) , 1/(2*N))).toPrecision(5);
+        getElement("out_c").innerHTML = `<img class="materialboxed" src="./bilinear_images/c.jpeg" /><br><br>Fc = ${Fc} rad/sec<br><br>`;
+
+        //       Normalized TF
+        getElement("out_t").innerHTML = `<img class="materialboxed" src="./tf/${N}.jpeg" /><br><img class="materialboxed" src="./tf/x.jpeg" /><br><br>`;
+    }
+    else
+    {   //       Order of the filter
+        var N = ((1/2)* Math.log( ( (1 / Math.pow(As,2)) - 1 ) / ( (1 / Math.pow(Ap,2)) - 1 ) ) / Math.log(omegaS/omegaP)).toPrecision(5);
+        getElement("out_o").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/n.jpeg" /><br><br>N = ${N} ~= ${Math.ceil(N)}<br><br>`;
+        N = Math.ceil(N);
+
+        //       Cut off Frequency
+        var Fc = (omegaP / Math.pow( ( (1 / Math.pow(Ap,2)) - 1 ) , 1/(2*N))).toPrecision(5);
+        getElement("out_c").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/c.jpeg" /><br><br>Fc = ${Fc} rad/sec<br><br>`;
+
+        //       Normalized TF
+        getElement("out_t").innerHTML = `<img class="materialboxed" src="./tf/${N}.jpeg" /><br><img class="materialboxed" src="./tf/x.jpeg" /><br><br>`;
+    }
+}
---
+++ Unit_3/Butterworth2/ux.js
@@ -0,0 +1,56 @@
+getElement("input0").addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      getElement("input1").focus();
+    }
+  });
+
+  getElement("input1").addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      getElement("input2").focus();
+    }
+  });
+
+  getElement("input2").addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      getElement("input3").focus();
+    }
+  });
+
+  getElement("input3").addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      getElement("input4").focus();
+    }
+  });
+
+  getElement("input4").addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      getElement("submit_button").click();
+      getElement("output_container").scrollIntoView();
+    }
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
+  });
