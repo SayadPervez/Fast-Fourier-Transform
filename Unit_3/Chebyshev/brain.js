88a7c47e7cfad51ebf8e5250121cfe9b6444d977
Sayad Pervez <pervez2504@gmail.com>
2022-01-31 17:16:37 +0530

    checby order complete

---
+++ Unit_3/Chebyshev/brain.js
@@ -0,0 +1,59 @@
+function getElement(id)
+{
+    return(document.getElementById(id));
+}
+
+function processData()
+{
+    getElement("output_container").scrollIntoView();
+    Ap = Number(getElement("input0").value);
+    As = Number(getElement("input1").value);
+    Wp = Number(getElement("input2").value);
+    Ws = Number(getElement("input3").value);
+    var T = Number(getElement("input4").value);
+    if(getElement("bilinear").checked)
+        transform = "Bilinear";
+    else if(getElement("impulseInvariant").checked)
+        transform = "Impulse Invariant";
+    else
+        getElement("output_container").innerHTML=`<h5 class="red-text bold">ERROR</h5>`
+    getElement("out_g").innerHTML=`<h6 class="indigo-text text-darken-4 bold">${transform} Transform</h6>Ap = ${Ap}<br><br>
+    As = ${As}<br><br>Wp = ${Wp}<br><br>Ws = ${Ws}<br><br>
+    `;
+    if(transform == "Bilinear")
+    {   //       Analog Frequency
+        omegaP = ((2/T)*Math.tan(Wp/2)).toPrecision(6);
+        omegaS = ((2/T)*Math.tan(Ws/2)).toPrecision(6);
+        getElement("out_a").innerHTML = `<img class="materialboxed" src="./bilinear_images/o.jpeg" /><br><br>Ωp = ${omegaP} rad/sec<br><br>Ωs = ${omegaS} rad/sec<br><br>`;
+
+        //       Order of the filter
+        var N = (Math.acosh( Math.pow(( (1 / Math.pow(As,2)) - 1 ),1/2) / Math.pow(( (1 / Math.pow(Ap,2)) - 1 ),1/2 )) / Math.acosh(omegaS/omegaP)).toPrecision(5);
+        getElement("out_o").innerHTML = `<img class="materialboxed" src="./bilinear_images/n.jpeg" /><br><br>N = ${N} ~= ${Math.ceil(N)}<br><br>`;
+        N = Math.ceil(N);
+
+        //       Cut off Frequency
+        var Fc = (omegaP / Math.pow( ( (1 / Math.pow(Ap,2)) - 1 ) , 1/(2*N))).toPrecision(5);
+        getElement("out_c").innerHTML = `<img class="materialboxed" src="./bilinear_images/c.jpeg" /><br><br>Ωc = ${Fc} rad/sec<br><br>`;
+
+        //       Normalized TF
+        getElement("out_t").innerHTML = `<img class="materialboxed" src="./tf/${N}.jpeg" /><br><img class="materialboxed" src="./tf/x.jpeg" /><br><br>`;
+    }
+    else
+    {   //       Analog Frequency
+        omegaP = (Wp/T).toPrecision(6);
+        omegaS = (Ws/T).toPrecision(6);
+        getElement("out_a").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/o.jpeg" /><br><br>Ωp = ${omegaP} rad/sec<br><br>Ωs = ${omegaS} rad/sec<br><br>`;
+
+        //       Order of the filter
+        var N = (Math.acosh( Math.pow(( (1 / Math.pow(As,2)) - 1 ),1/2) / Math.pow(( (1 / Math.pow(Ap,2)) - 1 ),1/2 )) / Math.acosh(omegaS/omegaP)).toPrecision(5);
+        getElement("out_o").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/n.jpeg" /><br><br>N = ${N} ~= ${Math.ceil(N)}<br><br>`;
+        N = Math.ceil(N);
+
+        //       Cut off Frequency
+        var Fc = (omegaP / Math.pow( ( (1 / Math.pow(Ap,2)) - 1 ) , 1/(2*N))).toPrecision(5);
+        getElement("out_c").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/c.jpeg" /><br><br>Ωc = ${Fc} rad/sec<br><br>`;
+
+        //       Normalized TF
+        getElement("out_t").innerHTML = `<img class="materialboxed" src="./tf/${N}.jpeg" /><br><img class="materialboxed" src="./tf/x.jpeg" /><br><br>`;
+    }
+}
