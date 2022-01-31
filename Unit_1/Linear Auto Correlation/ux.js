dd690135c4b586686346c5f503b81c1789f17775
Sayad Pervez <pervez2504@gmail.com>
2022-01-31 08:21:42 +0530

    linear correlations added

---
+++ Unit_1/Linear Auto Correlation/ux.js
@@ -0,0 +1,10 @@
+getElement("inputx").addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      getElement("submit_button").click();
+      getElement("output_container").scrollIntoView();
+    }
+  });
