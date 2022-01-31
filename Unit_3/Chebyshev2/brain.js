6f92e4d37132e9abf60390fcdaadcea88867c1ca
Sayad Pervez <pervez2504@gmail.com>
2022-01-31 17:48:16 +0530

    unit 3 fully completed

--- Unit_3/Chebyshev2/brain.js
+++ Unit_3/Chebyshev2/brain.js
@@ -8,8 +8,8 @@ function processData()
     getElement("output_container").scrollIntoView();
     Ap = Number(getElement("input0").value);
     As = Number(getElement("input1").value);
-    Wp = Number(getElement("input2").value);
-    Ws = Number(getElement("input3").value);
+    omegaP = Number(getElement("input2").value);
+    omegaS = Number(getElement("input3").value);
     var T = Number(getElement("input4").value);
     if(getElement("bilinear").checked)
         transform = "Bilinear";
@@ -18,14 +18,10 @@ function processData()
     else
         getElement("output_container").innerHTML=`<h5 class="red-text bold">ERROR</h5>`
     getElement("out_g").innerHTML=`<h6 class="indigo-text text-darken-4 bold">${transform} Transform</h6>Ap = ${Ap}<br><br>
-    As = ${As}<br><br>Wp = ${Wp}<br><br>Ws = ${Ws}<br><br>
+    As = ${As}<br><br>Ωp = ${omegaP}<br><br>Ωs = ${omegaS}<br><br>
     `;
     if(transform == "Bilinear")
-    {   //       Analog Frequency
-        omegaP = ((2/T)*Math.tan(Wp/2)).toPrecision(6);
-        omegaS = ((2/T)*Math.tan(Ws/2)).toPrecision(6);
-        getElement("out_a").innerHTML = `<img class="materialboxed" src="./bilinear_images/o.jpeg" /><br><br>Ωp = ${omegaP} rad/sec<br><br>Ωs = ${omegaS} rad/sec<br><br>`;
-
+    {
         //       Order of the filter
         var N = (Math.acosh( Math.pow(( (1 / Math.pow(As,2)) - 1 ),1/2) / Math.pow(( (1 / Math.pow(Ap,2)) - 1 ),1/2 )) / Math.acosh(omegaS/omegaP)).toPrecision(5);
         getElement("out_o").innerHTML = `<img class="materialboxed" src="./bilinear_images/n.jpeg" /><br><br>N = ${N} ~= ${Math.ceil(N)}<br><br>`;
@@ -37,11 +33,7 @@ function processData()
 
     }
     else
-    {   //       Analog Frequency
-        omegaP = (Wp/T).toPrecision(6);
-        omegaS = (Ws/T).toPrecision(6);
-        getElement("out_a").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/o.jpeg" /><br><br>Ωp = ${omegaP} rad/sec<br><br>Ωs = ${omegaS} rad/sec<br><br>`;
-
+    {
         //       Order of the filter
         var N = (Math.acosh( Math.pow(( (1 / Math.pow(As,2)) - 1 ),1/2) / Math.pow(( (1 / Math.pow(Ap,2)) - 1 ),1/2 )) / Math.acosh(omegaS/omegaP)).toPrecision(5);
         getElement("out_o").innerHTML = `<img class="materialboxed" src="./impulseinvariant_images/n.jpeg" /><br><br>N = ${N} ~= ${Math.ceil(N)}<br><br>`;
